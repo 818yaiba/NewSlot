d07dd8ca66b2f6c4f9c105e7dd7ab316c4cd2ec0
818yaiba <yaiba81ba@gmail.com>
2025-05-31 19:36:04 +0900

    PayLineクラスのドキュメンテーションを修正し、型チェックを強化

--- myapp/PayLine.py
+++ myapp/PayLine.py
@@ -1,6 +1,8 @@
+import GameData
+
+
 class PayLine:
-    """
-    入賞ライン
+    """入賞ライン
 
     Attributes
     ----------
@@ -23,24 +25,25 @@ class PayLine:
             入賞ライン名
         payline : tuple[int, int, int]
             入賞ライン [左リール, 中リール, 右リール]
-            (0:上段, 1:中段, 2:下段)
         """
         self._id: int = PayLine._Id
         PayLine._Id += 1
         self._name: str = name
 
-        if type(payline) is not tuple:
+        if not isinstance(payline, tuple):
             raise TypeError("入賞ラインの型が不正です")
         if len(payline) != 3:
             raise ValueError("入賞ラインの要素数がリール数と一致しません")
-        for pl in payline:
-            if type(pl) is not int:
-                raise TypeError("入賞ラインの型が不正です")
-            if pl not in [0, 1, 2]:
-                raise ValueError(
-                    "入賞位置指定が不正です (0:上段, 1:中段, 2:下段)"
-                )
-        self._payline: tuple[int, int, int] = payline
+        for paypos in payline:
+            if not isinstance(paypos, int):
+                raise TypeError("入賞位置の型が不正です")
+            if paypos not in [
+                GameData.REEL_POSITION_TOP,
+                GameData.REEL_POSITION_MIDDLE,
+                GameData.REEL_POSITION_BOTTOM,
+            ]:
+                raise ValueError("入賞位置の値が不正です")
+        self._payline = payline
 
     @property
     def id(self) -> int:
