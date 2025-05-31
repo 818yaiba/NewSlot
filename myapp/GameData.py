4f399a0a47f2fd0ea5f8909d94773af7a202a774
818yaiba <yaiba81ba@gmail.com>
2025-05-31 12:41:40 +0900

    色定義をRGB値から辞書形式に変更し、フレームレートの型をOptionalに更新

--- myapp/GameData.py
+++ myapp/GameData.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 # ゲームデータフォルダ名
 GAMEDATA_FOLDER_NAME: str = "assets"
@@ -10,7 +11,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # 1つ上の階層のディレクトリパスを取得
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 # ゲーム画像ディレクトリ
-IMAGE_DIRECTORY: str = os.path.join(parent_dir, GAMEDATA_FOLDER_NAME, IMAGE_FOLDER_NAME)
+IMAGE_DIRECTORY: str = os.path.join(
+    parent_dir, GAMEDATA_FOLDER_NAME, IMAGE_FOLDER_NAME
+)
 
 # ゲーム画面幅
 SCREEN_WIDTH: int = 800
@@ -21,9 +24,9 @@ GAME_TITLE: str = "test"
 
 # フォント
 FONT_WANPAKURUIKA = "わんぱくルイカ-０７"
-
 # フレームレート指定
-FRAMERATE: int | None = None
+
+FRAMERATE: Optional[int] = None
 # より高品質な時間管理(CPU使用率増加)
 HIGHQUALITY_TIMECALC: bool = True
 
@@ -58,26 +61,31 @@ BET_INTERVAL: float = (1 / 30) * 2
 REELWAIT_TIME: float = 4.1
 
 # 色
-RGB_WHITE: tuple[int, int, int] = (255, 255, 255)
-RGB_BLACK: tuple[int, int, int] = (0, 0, 0)
-RGB_SILVER: tuple[int, int, int] = (192, 192, 192)
-RGB_GRAY: tuple[int, int, int] = (128, 128, 128)
-RGB_RED: tuple[int, int, int] = (255, 0, 0)
-RGB_MAROON: tuple[int, int, int] = (128, 0, 0)
-RGB_YELLOW: tuple[int, int, int] = (255, 255, 0)
-RGB_OLIVE: tuple[int, int, int] = (128, 128, 0)
-RGB_LIME: tuple[int, int, int] = (0, 255, 0)
-RGB_GREEN: tuple[int, int, int] = (0, 128, 0)
-RGB_AQUA: tuple[int, int, int] = (0, 255, 255)
-RGB_TEAL: tuple[int, int, int] = (0, 128, 128)
-RGB_BLUE: tuple[int, int, int] = (0, 0, 255)
-RGB_NAVY: tuple[int, int, int] = (0, 0, 128)
-RGB_FUCHSIA: tuple[int, int, int] = (255, 0, 255)
-RGB_PURPLE: tuple[int, int, int] = (128, 0, 128)
+COLORS: dict[str, tuple[int, int, int]] = {
+    "WHITE": (255, 255, 255),
+    "BLACK": (0, 0, 0),
+    "SILVER": (192, 192, 192),
+    "GRAY": (128, 128, 128),
+    "RED": (255, 0, 0),
+    "MAROON": (128, 0, 0),
+    "YELLOW": (255, 255, 0),
+    "OLIVE": (128, 128, 0),
+    "LIME": (0, 255, 0),
+    "GREEN": (0, 128, 0),
+    "AQUA": (0, 255, 255),
+    "TEAL": (0, 128, 128),
+    "BLUE": (0, 0, 255),
+    "NAVY": (0, 0, 128),
+    "FUCHSIA": (255, 0, 255),
+    "PURPLE": (128, 0, 128),
+}
 
-
-# ゲーム定数
-# 最小有効BET数
+# 最小有効BET数: プレイヤーが行える最小のBET数を定義します。
+# これにより、ゲーム内でのBETが1未満になることを防ぎます。
 VALIDBET_MIN: int = 1
+
+# 最大有効BET数: プレイヤーが行える最大のBET数を定義します。
+# これにより、ゲーム内でのBETが3を超えることを防ぎます。
+VALIDBET_MAX: int = 3
 # 最大有効BET数
 VALIDBET_MAX: int = 3
