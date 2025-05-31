1489aa88c87c2d53075b940e314532f606458a59
818yaiba <yaiba81ba@gmail.com>
2025-05-31 19:38:27 +0900

    図柄画像の読み込みエラーハンドリングを改善し、ファイルが見つからない場合の例外をFileNotFoundErrorからValueErrorに変更

--- myapp/Symbol.py
+++ myapp/Symbol.py
@@ -41,13 +41,8 @@ class Symbol:
         # 図柄画像読み込み
         imagefile_path = os.path.join(GameData.IMAGE_DIRECTORY, filename)
         self._image = cv2.imread(imagefile_path)
-
-        if self._image is None:  # 画像が読み込めなかった場合
-            raise FileNotFoundError(
-                f"図柄画像ファイルが見つかりません: {imagefile_path}"
-            )
-
-        # 図柄画像チェック
+        if self._image is None:
+            raise ValueError("図柄画像がNoneです")
         if self._image.shape[1] != GameData.SYMBOL_WIDTH:
             raise ValueError("図柄画像の幅が既定値と一致しません")
         if self._image.shape[0] != GameData.SYMBOL_HEIGHT:
