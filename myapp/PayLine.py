9df995a1bb347ccccc5cc947dcc8ebb027f804c2
818yaiba <yaiba81ba@gmail.com>
2025-06-04 21:23:29 +0900

    PayLineクラスの属性名を整理し、コンストラクタの引数名を統一。ドキュメンテーションを改善。

--- myapp/PayLine.py
+++ myapp/PayLine.py
@@ -7,43 +7,43 @@ class PayLine:
     Attributes
     ----------
     id : int
-        入賞ラインID
+        ID
     payline : tuple[int, int, int]
-        入賞ライン [左リール, 中リール, 右リール]
+        ライン [左リール, 中リール, 右リール]
         (0:上段, 1:中段, 2:下段)
     """
 
     _Id: int = 0
 
-    def __init__(self, payline: tuple[int, int, int]) -> None:
+    def __init__(self, line: tuple[int, int, int]) -> None:
         """
         Parameters
         ----------
-        payline : tuple[int, int, int]
-            入賞ライン [左リール, 中リール, 右リール]
+        line : tuple[int, int, int]
+            ライン [左リール, 中リール, 右リール]
         """
         self._id: int = PayLine._Id
         PayLine._Id += 1
 
-        if not isinstance(payline, tuple):
+        if not isinstance(line, tuple):
             raise TypeError("入賞ラインの型が不正です")
-        if len(payline) != 3:
+        if len(line) != 3:
             raise ValueError("入賞ラインの要素数がリール数と一致しません")
-        for paypos in payline:
-            if not isinstance(paypos, int):
+        for reel_pos in line:
+            if not isinstance(reel_pos, int):
                 raise TypeError("入賞位置の型が不正です")
-            if paypos not in [
+            if reel_pos not in [
                 GameData.REEL_POSITION_TOP,
                 GameData.REEL_POSITION_MIDDLE,
                 GameData.REEL_POSITION_BOTTOM,
             ]:
                 raise ValueError("入賞位置の値が不正です")
-        self._payline = payline
+        self._payline = line
 
     @property
     def id(self) -> int:
         return self._id
 
     @property
-    def payline(self) -> tuple[int, int, int]:
+    def line(self) -> tuple[int, int, int]:
         return self._payline
